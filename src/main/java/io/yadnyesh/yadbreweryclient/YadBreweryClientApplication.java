f6bf582f56e8341e394f6641af5c57bce23d044b
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-04-02 14:30:04 +0530

    First commit

---
+++ src/main/java/io/yadnyesh/yadbreweryclient/YadBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.yadbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YadBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YadBreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yadnyesh/yadbreweryclient/YadBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.yadbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YadBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
